ac7756814b7efc6cbdc98b9a3c66d258a8655eba
hcvin <hvinson2@student.cccs.edu>
2023-03-25 19:26:09 -0600

    Bug fix, updated path and removed unnecessary flayer code.

--- assignment_7/exercise1.py
+++ assignment_7/exercise1.py
@@ -1,5 +1,5 @@
 import arcpy
-arcpy.env.workspace = r"C:\Users\David Neufeld\Documents\ArcGIS\GIS305\Data\Admin\AdminData.gdb"
+arcpy.env.workspace = r"C:\Users\hcvin\OneDrive\Desktop\Spring_2023\GIS_3005\week1\Admin\AdminData.gdb"
 arcpy.env.overwriteOutput = True
 arcpy.SelectLayerByAttribute_management("cities", "CLEAR_SELECTION")
 
@@ -16,10 +16,11 @@ arcpy.management.SelectLayerByLocation(flayer, "WITHIN_A_DISTANCE", "us_rivers",
 my_cnt = arcpy.management.GetCount(flayer)
 print(f"Selected cities is: {my_cnt}")
 
-flayer = arcpy.MakeFeatureLayer_management("cities", "Cities_Layer")
+
 field = 'POP1990'
 total = 0
 i = 1
+
 with arcpy.da.SearchCursor(flayer, field) as cursor:
     for row in cursor:
         print(i, str(row[0]))
